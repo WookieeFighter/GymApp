750cbb33e8dbe63a40f4d7222bf95baf5ffdd9b0
Eric Walker <eric.walker.kin@gmail.com>
2016-08-02 19:48:53 -0400

    firstcommit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+	
+</footer> 
+<!-- SCRIPTS -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+<script type="text/javascript" src="app/app.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GymApp</title>
+
+    <!-- CSS -->
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <!--Import style.css-->
+    <link href="css/style.css" rel="stylesheet">
+   </head>
+
+ <body>
+
+ <header>
+</header>
